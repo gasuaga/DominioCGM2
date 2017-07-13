af9fbfb8ef6153fe6bc25e730132c6b925548dc1
gabriel asuaga <bizzoasuaga@hotmail.com>
2017-07-13 16:23:07 -0300

    Primero cambios de el dominio comentado

---
+++ Dominio/Marcador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iMacros; // usign del controlador de imacros
+namespace Dominio
+{
+    /**
+     * @class   Marcador
+     *
+     * @brief   Es la clase relacionada con todas las tareas de los 
+     *          marcadores dentro de inconsert, tanto eliminar insertar modificar
+     *          lotes sobre uno o mas marcadores (Motores de marcacion)
+     *
+     * @author  WINMACROS
+     * @date    13/07/2017
+     */
+
+    class Marcador
+    {
+
+
+    }
+}
--- Dominio/Sistema.cs
+++ Dominio/Sistema.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    public class Class1
+    class Sistema
     {
     }
 }
