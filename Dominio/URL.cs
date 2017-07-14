a1209877f72a1d1f58198e978558867b08836a45
gabriel asuaga <bizzoasuaga@hotmail.com>
2017-07-14 16:50:50 -0300

    Se agrega la referencia para HttpPostedFile dentro del dominio

---
+++ Dominio/URL.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    /**
+     * @class   URL
+     *
+     * @brief   Es quien contiene toda la 
+     *          informacion sobre la 
+     *          url del excel
+     *
+     * @author  WINMACROS
+     * @date    14/07/2017
+     */
+
+    class URL
+    {
+        #region propery
+        public string Direccion { get; set; }
+        public string Nombre { get; set; }
+        public string Extencion { get; set; }
+        #endregion
+
+        #region constructores
+        public URL(HttpPostedFile pDireccion)
+        {
+            Sistema sis = Sistema.Sis;
+            string nombreTemp = Path.GetFileName(pDireccion.FileName);
+            Nombre = nombreTemp.Substring(0, nombreTemp.Length - 4);
+            Extencion = Path.GetExtension(nombreTemp);
+            Direccion = sis.urlDataSourcer;
+            try
+            {
+                if (File.Exists(Direccion + Nombre + Extencion))
+                    File.Delete(Direccion + Nombre + Extencion);
+                pDireccion.SaveAs(Direccion + Nombre + Extencion);
+            }
+            catch (Exception)
+            {
+                new URL(Nombre + 1, pDireccion);
+            }
+        }
+        public URL(string pNombre, string pDireccion)
+        {
+            Nombre = pNombre.Substring(0, pNombre.Length - 4);
+            Direccion = pDireccion;
+            Extencion = pNombre.Substring(pNombre.Length - 4);
+        }
+        public URL(string pNombre, HttpPostedFile pDireccion)
+        {
+            Sistema sis = Sistema.Sis;
+            string nombreTemp = Path.GetFileName(pDireccion.FileName);
+            Nombre = pNombre;
+            Extencion = Path.GetExtension(nombreTemp);
+            Direccion = sis.urlDataSourcer;
+            try
+            {
+                if (File.Exists(Direccion + Nombre + Extencion))
+                    File.Delete(Direccion + Nombre + Extencion);
+                pDireccion.SaveAs(Direccion + Nombre + Extencion);
+            }
+            catch (Exception)
+            {
+                new URL(Nombre + 1, pDireccion);
+            }
+        }
+        #endregion
+
+    }
+}
