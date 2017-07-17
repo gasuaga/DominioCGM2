a303e418e98fc590597db73ae046feae15ff3bae
gabriel asuaga <bizzoasuaga@hotmail.com>
2017-07-17 09:45:26 -0300

    Se cambiaron de lugar algunas funciones, se termino de crear la clase sistema con sus respectivos comentarios

--- Dominio/Lote.cs
+++ Dominio/Lote.cs
@@ -267,6 +267,165 @@ namespace Dominio
             while (hubo_cambio);
         }
 
+        /**
+         * @fn  public void crearNuevaImportacion()
+         *
+         * @brief   Carga el lote a inconsert.
+         *
+         * @author  WINMACROS
+         * @date    17/07/2017
+         */
+
+        public void crearNuevaImportacion()
+        {
+            Sistema s = Sistema.Sis;
+            s.accionesCodigo("Inicio crear nueva importacion");
+            int ban = 0;
+            baseDatos bd = baseDatos.Bd;
+            do
+            {
+                s.ejecutarMacro(s.m_app, "nombreLoteC", Nombre);
+                s.ejecutarMacro(s.m_app, s.direccion + "Cargar lote/NuevaImportacion1.iim"
+                                , "Crear nueva imporatcion1", true);
+
+                s.ejecutarMacro(s.m_app, "urlLoteC", Exc.Direccion.ToString());
+
+                s.ejecutarMacro(s.m_app, "TAG POS=1 TYPE=INPUT:FILE ATTR=ID:btnAddFile CONTENT= {{urlLoteC}}"
+                                , "Poner la url en el inpur file", false);
+
+                s.ejecutarMacro(s.m_app, s.direccion + "Cargar lote/NuevaImportacion2.iim"
+                                , "Crear nueva imporatcion2", true);
+                do
+                {
+                    ban = comprobarEstadoImportacion();
+
+                } while (ban != 1 && ban != -1);
+
+            } while (ban != 1);
+
+            bd.cambiarEstadoLote(this);
+        }
+
+        /**
+         * @fn  private int comprobarEstadoImportacion(int pPos)
+         *
+         * @brief   Una vez importado se comprueba 
+         *          si el lote se esta cargado, esta
+         *          completo o fue abortado.
+         *
+         * @author  WINMACROS
+         * @date    17/07/2017
+         
+         * @return  Estado del lote.
+         */
+
+        private int comprobarEstadoImportacion()
+        {
+            String nombreLote;
+            String estaodLote;
+            Sistema s = Sistema.Sis;
+            int ban = 0;
+            int pos;
+            do
+            {
+                nombreLote = "";
+                estaodLote = "";
+                pos = 2;
+                do
+                {
+                    s.ejecutarMacro(s.m_app, "nombreLoteC", Nombre);
+                    s.ejecutarMacro(s.m_app, "posC", pos.ToString());
+                    s.ejecutarMacro(s.m_app, s.direccion + "Cargar lote/ComprobarEstadoLote.iim"
+                                    , "Comprobar estado lote", true);//extrae el nombre y el estado del 
+                                                                    //lote
+                    estaodLote = s.ejecutarMacroExtract(s.m_app, 2, "Estado del lote: " + Nombre);
+                    nombreLote = s.ejecutarMacroExtract(s.m_app, 1, "Nomre del lote en la pocicion" 
+                                                        + pos.ToString());
+                    if (nombreLote == "#EANF#" || nombreLote == "NODATA")//si el nombrre no dio nada 
+                        return -1;//retorna -1 se importa denuevo
+                    pos++;
+                    if (pos > 10)
+                        pos = 0;
+                } while (nombreLote != Nombre);//prueba hasta qe los nombres sean iguales 
+                                                //(nombre del lote igual lombre de la imporacion)
+
+                ban = Lote.estadoLoteImportacion(estaodLote);//es un numero que denomina el estado
+                                                             //de la imporacion
+                s.accionesCodigo("Estado lote importacion: ", ban.ToString());
+
+                if (ban == -2) // si el lote no esta que lo importe de nuevo
+                    return -1;
+                else if (ban == -1) //si lo aborto
+                {
+                    eliminarImportacion(0);//elimina la imporacion
+                    return -1;
+                }
+                else if (ban == 1)
+                    Estado = Lote.tipoEstado.Importado;//Imporado
+
+            } while (ban == 2);//2 es en ejecucuin
+            return ban;
+        }
+
+        /**
+         * @fn  public void eliminarImportacion(int pCantidad)
+         *
+         * @brief   Elimina la imporacion de inconsert.
+         *
+         * @author  WINMACROS
+         * @date    17/07/2017
+         *
+         * @param   pCantidad   La cantidad de intentos que tomo
+         *                      para eliminar la imporacion.
+         */
+
+        public void eliminarImportacion(int pCantidad)
+        {
+            Sistema s = Sistema.Sis;
+            int ban = eliminarImporacionAux();
+            s.accionesCodigo("Intento n." + pCantidad + " de eliminacion de lote:" + Nombre, ban.ToString());
+            baseDatos bd = baseDatos.Bd;
+            if (pCantidad < 2)
+            {
+                if (ban == 0)
+                    eliminarImportacion(pCantidad + 1);
+                else
+                {
+                    s.ejecutarMacro(s.m_app, "WAIT SECONDS=10", "Esperar 10s" + false);
+                    Estado = Lote.tipoEstado.Eliminado;
+                }
+            }
+            else
+                Estado = Lote.tipoEstado.ErrorEliminacion;
+            bd.cambiarEstadoLote(this);
+        }
+
+        /**
+         * @fn  private int eliminarImporacionAux()
+         *
+         * @brief   El metodo que llama a las macros
+         *          para eliminar la imporacion.
+         *
+         * @author  WINMACROS
+         * @date    17/07/2017
+         *
+         * @return  El estado de la imporacion que 
+         *          intento eliminar.
+         */
+
+        private int eliminarImporacionAux()
+        {
+            int ban = 0;
+            Sistema s = Sistema.Sis;
+            s.ejecutarMacro(s.m_app, "Lote", Nombre);
+            s.ejecutarMacro(s.m_app, s.direccion + "eliminar lote/EliminarLoteImportacion.iim", "Eliminar Importacion ", true);
+            s.ejecutarMacro(s.m_app, "nombreLoteC", Nombre);
+            s.ejecutarMacro(s.m_app, s.direccion + "Cargar lote/ComprobarEstadoLote.iim", "Comprobar estado importacion eliminada", true);
+            String estaodLote = s.ejecutarMacroExtract(s.m_app, 0, "Estado del lote: " + Nombre);
+            ban = Lote.estadoLoteParaEliminar(estaodLote);
+            return ban;
+        }
+
         #region overrides
         public override bool Equals(object obj)
         {
